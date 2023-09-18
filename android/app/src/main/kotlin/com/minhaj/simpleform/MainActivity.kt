9165d80c169375e1ec4fcce3b33263680894ce6f
Minhaj Mahim <minhaj.thecoder@gmail.com>
2023-09-18 21:55:22 +0600

    Initial commit

---
+++ android/app/src/main/kotlin/com/minhaj/simpleform/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.minhaj.simpleform
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(simpleFormApp());
+}
+class simpleFormApp extends StatelessWidget{
+  const simpleFormApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeActivity(),
+    );
+  }
+
+}
+class HomeActivity extends StatelessWidget{
+   HomeActivity({super.key});
+
+  ButtonStyle buttonStyle = ElevatedButton.styleFrom(
+    minimumSize: Size(double.infinity, 60),
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Simple Form"),
+        backgroundColor: Colors.green,
+        foregroundColor: Colors.black,
+        centerTitle: true,
+        toolbarHeight: 70,
+        elevation: 10,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(10),
+            child: TextField(
+              decoration: InputDecoration(
+                labelText: "First Name",
+                border: OutlineInputBorder(),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(10),
+            child: TextField(
+              decoration: InputDecoration(
+                labelText: "Last Name",
+                border: OutlineInputBorder(),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(10),
+            child: TextField(
+              decoration: InputDecoration(
+                labelText: "E-mail Address",
+                border: OutlineInputBorder(),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(15),
+            child: ElevatedButton(onPressed: (){},child: Text("Submit"), style: buttonStyle),
+          ),
+        ],
+      ),
+    );
+  }
+
+}
